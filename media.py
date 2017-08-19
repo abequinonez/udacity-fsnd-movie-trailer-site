ed901dc4536c4642c4defe2f716613530c3a91e9
abequinonez <abequinonez@users.noreply.github.com>
2017-08-18 21:24:42 -0700

    Modified comment and removed an extra space

--- media.py
+++ media.py
@@ -1,14 +1,14 @@
 class Movie():
 	"""This class stores movie related information."""
 
-	# Class variable containing valid movie ratings. Called by each movie
+	# Class variable containing valid movie ratings. Referenced by each movie
 	# object during instantiation.
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 	
 	# Constructs an instance of the Movie class and assigns argument values
 	# to the instance variables
 	def __init__(self, title, poster_image, trailer_youtube, release_date,
-				 director, genre, mpaa_rating, imdb_rating):
+				director, genre, mpaa_rating, imdb_rating):
 		self.title = title
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
